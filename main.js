68dd90e374a46f23c9d15d7b30b3e3e1def5e202
trina314 <trina0dang@gmail.com>
2024-01-15 18:29:58 -0600

    boop

--- main.js
+++ main.js
@@ -6,12 +6,23 @@ function boop() {
     let resource; //+ Math.floor(Math.random() * 2);
     let numAffixes;
     let chosenAffixes = [];
+    const boardSizes = new Map([
+      ["A", [11,11]],
+      ["B", [13,13]],
+      ["C", [15,15]],
+      ["D", [15,15]],
+      ["E", [15,15]],
+      ["F", [15,15]],
+      ["G", [11, 20]]
+    ]);
 
     //const biomes = ["Arid Wasteland", "Lush Rainforest", "Frozen Tundra", "Volcanic Caldera", "Underwater Abyss", 
     //                "Crystalline Caves", "Toxic Swamp", "Mountainous Badlands", "Subterranean Tunnels"];
-    const Affixes = ["Harsh Habitality", "Infections & Insects", "Lmited Visibility", "Electronics Malfunctions", "Limited Communication", 
-                      "Gentle Giants", "Corrosive Environment", "Biomimicry", "Carnivorous Flora", "Weak Structures", "Malleable Landscape",
-                      "Rich Deposits", "Abundant Specimens", "Other Crews’ Remnants"];
+    const Affixes = ["Harsh Habitality", "Infections & Insects", "Lmited Visibility", 
+                    "Electronics Malfunctions", "Limited Communication", "Gentle Giants",
+                    "Corrosive Environment", "Biomimicry", "Carnivorous Flora", 
+                    "Weak Structures", "Malleable Landscape", "Rich Deposits", "Abundant Specimens", 
+                    "Other Crews’ Remnants"];
 
     let biomes = [];
 
@@ -122,29 +133,71 @@ function boop() {
     //set up board size and populate with items
     let board = Math.floor(Math.random()*biome.boardSize.length);
     board = biome.boardSize[board];
-    size = 7; //change later based on board size
+    size = boardSizes.get(board); //height x width. change later based on board size
     let resources = [];
     let row;
     let column;
 
-    //clear board
-    for(let i = 1; i < 8; i ++){
-      for(let j = 65; j < 72; j ++){
+    //clear board and make it to size
+    for(let i = 1; i < 21; i ++){
+      for(let j = 65; j < 80; j ++){
         document.getElementById(String.fromCharCode(j) + i).innerHTML = "";
       }
     }
 
-    //populate board with resources
+    //making right rows appear and disappear
+    for(let i = 76; i < 80; i ++){
+      document.getElementById("row" + String.fromCharCode(i)).style.display = "";
+    }
+    for(let i = 65 + size[0]; i < 80; i ++){
+      document.getElementById("row" + String.fromCharCode(i)).style.display = "None";
+    }
+
+    //add and remove column cells
+    for(let i = 11; i <= size[1]; i ++){
+      for(let j = 65; j < 65 + size[0]; j ++){
+        document.getElementById(String.fromCharCode(j) + i).style.display = "";
+      }
+    }
+    for(let i = size[1] + 1; i < 21; i ++){
+      for(let j = 65; j < 65 + size[0]; j ++){
+        document.getElementById(String.fromCharCode(j) + i).style.display = "None";
+      }
+    }
+
+    //make right header cells appear and dissappear
+    for(let i = 11; i <= size[1]; i ++){
+      document.getElementById("num" + i).style.display = "";
+    }
+    for(let i = size[1] + 1; i < 21; i ++){
+      document.getElementById("num" + i).style.display = "None";
+    }
+
+
+    // for(let i = 12; i < size[1]; i ++){
+    //   for(let j = 75; j < 65 + size[0]; j ++){
+    //     // document.getElementById(String.fromCharCode(j) + i).style.display = "";
+    //     document.getElementById(String.fromCharCode(j) + i).innerHTML = "";
+    //   }
+    // }
+
+
+
+
+    // //populate board with resources
     for(let i = resource + 5; i > 0; i --){
-      row = String.fromCharCode(Math.floor(Math.random()*size) + 65);
-      column = Math.floor(Math.random()*size) + 1;
+      row = String.fromCharCode(Math.floor(Math.random()*size[0]) + 65).toUpperCase();
+      column = Math.floor(Math.random()*size[1]) + 1;
+
       while(document.getElementById(row + column).innerHTML != ""){
-        row = String.fromCharCode(Math.floor(Math.random()*size) + 65);
-        column = Math.floor(Math.random()*size) + 1;
+        row = String.fromCharCode(Math.floor(Math.random()*size[0]) + 65).toUpperCase();
+        column = Math.floor(Math.random()*size[1]) + 1;
       }
       resources.push(new Item("*", row + column));
       document.getElementById(row + column).innerHTML = resources[resources.length - 1].symbol;
     }
+
+    //remove unused rows and columns
 
     //change text to info
     document.getElementById("biome").innerHTML = biome.name;
@@ -153,7 +206,6 @@ function boop() {
     document.getElementById("resource").innerHTML = resource;
     document.getElementById("hazard").innerHTML = hazard;
     document.getElementById("gameBoards").innerHTML = board;
-
   }
 
   function Biome(name, effect, affix, boardSize) {
